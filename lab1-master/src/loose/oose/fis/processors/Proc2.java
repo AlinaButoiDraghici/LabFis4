f38827ab3729f4e75fea78811e476a46c4db3124
Alina <butoidraghicialina@gmail.com>
2021-03-13 16:46:26 +0200

    Added and modified classes

---
+++ lab1-master/src/loose/oose/fis/processors/Proc2.java
@@ -0,0 +1,10 @@
+package loose.oose.fis.processors;
+
+import loose.oose.fis.documents.Document;
+
+import java.util.ArrayList;
+
+public interface Proc2 extends Procesor{
+    int proceseaza(ArrayList<Document> documente);
+    int varrr=0; //am adaugat o cls si o var
+}
